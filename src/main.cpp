62880641ac8a43b60c5ae2215a61f7cf0bcba892
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2026-02-13 21:40:30 +0100

    feat: simple buffer rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,72 @@
-#include <iostream>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <csignal>
 #include <string>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-  string message = "Hello world";
+static termios g_orig;
 
-  cout << message << std::endl;
+static void disableRawMode() {
+  tcsetattr(STDERR_FILENO, TCSAFLUSH, &g_orig);
+}
+
+static void die(string msg) {
+  cerr << msg << ": " << strerror(errno) << endl;
+  exit(1);
+}
+
+static void enableRawMode() {
+  if (tcgetattr(STDERR_FILENO, &g_orig) == -1) die("tcsetattr");
+
+  atexit(disableRawMode);
+
+  termios raw = g_orig;
+
+  raw.c_lflag &= ~(ICANON | ECHO | ISIG);
+  
+  raw.c_iflag &= ~(IXON | ICRNL);
+
+  raw.c_cc[VMIN] = 1;
+  raw.c_cc[VTIME] = 0;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+int main() {
+  enableRawMode();
+
+  cout << "Raw mode ON. Press 'q' to quit" << endl;
+
+  string buffer = "";
+
+  while(true) {
+    char c;
+
+    ssize_t n = read(STDERR_FILENO, &c, 1);
+    if (n == -1) die("read");
+
+    if (c == 'q') {
+      cout << "\nExiting..." << endl;
+      break;
+    }
+
+    if (c >= 32 && c <= 126) {
+      buffer.push_back((int)(unsigned char)c);
+    } 
+
+    if (buffer.length() != 0 && c == 127)  {
+      buffer.pop_back();
+    }
+
+    cout << buffer << endl;
+  }
 
   return 0;
 }
